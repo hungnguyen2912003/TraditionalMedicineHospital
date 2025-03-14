5ab30bb86eb4eafdc57324f16a68fa2d01f822bf
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-14 18:21:55 +0700

    Create MedicineCategory.cs

---
+++ Project/Areas/Admin/Models/Entities/MedicineCategory.cs
@@ -0,0 +1,23 @@
+﻿using Project.Areas.Admin.Models.Commons;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Areas.Admin.Models.Entities
+{
+    [Table("MedicineCategory")]
+    public class MedicineCategory : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Code { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(500)]
+        public string? Description { get; set; }
+        [Required]
+        public string Image { get; set; }
+    }
+}
