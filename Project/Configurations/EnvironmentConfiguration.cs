da445c3cf9e5251bcd6d1279dbf1be446dee956b
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-14 23:26:25 +0700

    organize program.cs

---
+++ Project/Configurations/EnvironmentConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Project.Configurations
+{
+    public static class EnvironmentConfiguration
+    {
+        public static WebApplication ConfigureEnvironment(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            return app;
+        }
+    }
+}
---
+++ Project/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Configurations
+{
+    public static class ServiceConfiguration
+    {
+        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews();
+            return builder;
+        }
+    }
+}
