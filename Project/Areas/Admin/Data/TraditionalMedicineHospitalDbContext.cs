02e10991a8910a6c29bfafe04b5df59f9fdd668a
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-14 23:55:20 +0700

    add migration connection string

---
+++ Project/Areas/Admin/Data/TraditionalMedicineHospitalDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Areas.Admin.Models.Entities;
+
+namespace Project.Areas.Admin.Data
+{
+    public class TraditionalMedicineHospitalDbContext : DbContext
+    {
+        public TraditionalMedicineHospitalDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<MedicineCategory> medicineCategories { get; set; }
+        DbSet<Medicine> medicines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Medicine>()
+                .Property(e => e.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
