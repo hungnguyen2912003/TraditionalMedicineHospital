e34a40a104fc0e46c9526bbdfb7583526f677368
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-14 18:40:23 +0700

    add relationship medine and category

---
+++ Project/Areas/Admin/Models/Entities/Medicine.cs
@@ -0,0 +1,51 @@
+﻿using Project.Areas.Admin.Models.Commons;
+using Project.Areas.Admin.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Areas.Admin.Models.Entities
+{
+    [Table("Medicine")]
+    public class Medicine : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Code { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Images { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int InStock { get; set; }
+        [Required]
+        public int UnitQuantity { get; set; }
+        [Required]
+        [StringLength(10)]
+        public UnitType UnitType { get; set; }
+        [Required]
+        public decimal DosageQuantity { get; set; }
+        [Required]
+        [StringLength(10)]
+        public DosageUnit DosageUnit { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Manufacturer { get; set; }
+        [Required]
+        public DateTime ManufacturedDate { get; set; }
+        [Required]
+        public string ActiveIngredient { get; set; }
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+        public Guid MedicineCategoryId { get; set; }
+
+        /////////////////////////////////////////////////////
+        /// Relationship
+        [ForeignKey("MedicineCategoryId")]
+        public virtual MedicineCategory? MedicineCategory { get; set; }
+    }
+}
