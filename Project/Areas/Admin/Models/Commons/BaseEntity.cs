ebdb7d81b81bd64868d9968e92cf9950164bdc0e
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-14 18:15:59 +0700

    Create base entity class

---
+++ Project/Areas/Admin/Models/Commons/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Areas.Admin.Models.Commons
+{
+    public abstract class BaseEntity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
