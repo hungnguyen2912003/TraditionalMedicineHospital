96470a483456fc8399e76a7ce93dc029a039329c
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-14 18:40:37 +0700

    add enum medicine

---
+++ Project/Areas/Admin/Models/Enums/DosageUnit.cs
@@ -0,0 +1,12 @@
+﻿namespace Project.Areas.Admin.Models.Enums
+{
+    public enum DosageUnit
+    {
+        Tablet = 1,
+        Capsule = 2,
+        Bottle = 3,
+        Tube = 4,
+        Box = 5,
+        Pack = 6
+    }
+}
