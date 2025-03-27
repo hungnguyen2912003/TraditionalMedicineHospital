ab24468af6726528a49f0108ae5096e76aaead73
hungnguyen2912003 <hungthinh291@gmail.com>
2025-03-27 12:47:35 +0700

    create Medicine Controller

---
+++ Project/Areas/Admin/Controllers/MedicinesController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Project.Repositories.Interfaces;
+using Project.Services.Interfaces;
+
+namespace Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MedicinesController : Controller
+    {
+        private readonly IMedicineRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly IImageService _service;
+        public MedicinesController
+        (
+            IMedicineRepository repository,
+            IMapper mapper,
+            IImageService service
+        )
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var list = await _repository.GetAllAsync();
+            var activeList = list.Where(x => x.Status == true).ToList();
+            return View(activeList);
+        }
+    }
+}
