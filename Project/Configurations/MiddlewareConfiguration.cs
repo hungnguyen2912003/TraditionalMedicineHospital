9fb2d0e671a588591e01b60baf7895ce8952865c
hungnguyen2912003 <hungthinh291@gmail.com>
2025-04-05 01:02:56 +0700

    Add FE Error

--- Project/Configurations/MiddlewareConfiguration.cs
+++ Project/Configurations/MiddlewareConfiguration.cs
@@ -2,6 +2,10 @@
 {
     public static class MiddlewareConfiguration
     {
-
+        public static WebApplication ConfigureMiddleware(this WebApplication app)
+        {
+            app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            return app;
+        }
     }
 }
---
+++ Project/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HandleError(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    return View("Error404");
+                case 500:
+                    return View("Error500");
+                case 503:
+                    return View("Error503");
+                default:
+                    return View("Error");
+            }
+        }
+    }
+}
