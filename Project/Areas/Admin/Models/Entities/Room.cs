31a2e535dd9d7ccb02679ea1a2a6e59c829ab890
hungnguyen2912003 <hungthinh291@gmail.com>
2025-04-04 20:06:41 +0700

    Initial Room Tbl

---
+++ Project/Areas/Admin/Models/Entities/Room.cs
@@ -0,0 +1,20 @@
+﻿using Project.Areas.Admin.Models.Commons;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Areas.Admin.Models.Entities
+{
+    [Table("Room")]
+    public class Room : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Code { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
