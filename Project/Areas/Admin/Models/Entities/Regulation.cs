3bba89110693cbd426637616fc6257ceeabc5609
hungnguyen2912003 <hungthinh291@gmail.com>
2025-04-04 23:04:02 +0700

    Create Regulation Tbl

---
+++ Project/Areas/Admin/Models/Entities/Regulation.cs
@@ -0,0 +1,24 @@
+﻿using Project.Areas.Admin.Models.Commons;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Areas.Admin.Models.Entities
+{
+    [Table("Regulation")]
+    public class Regulation : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Code { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public string? Description { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
