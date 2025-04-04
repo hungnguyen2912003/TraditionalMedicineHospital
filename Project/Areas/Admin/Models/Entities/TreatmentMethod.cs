f78a43a51d6bc59ebd37d2d9107f354d8c3d1063
hungnguyen2912003 <hungthinh291@gmail.com>
2025-04-04 12:34:48 +0700

    Initial TreatmentMethob tbl

---
+++ Project/Areas/Admin/Models/Entities/TreatmentMethod.cs
@@ -0,0 +1,30 @@
+﻿using Project.Areas.Admin.Models.Commons;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Areas.Admin.Models.Entities
+{
+    [Table("TreatmentMethod")]
+    public class TreatmentMethod : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Code { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public decimal Cost { get; set; }
+
+        //// ForeignKey
+        //public Guid DepartmentId { get; set; }
+
+        ///////////////////////////////////////////////////////
+        ///// Relationships
+        //[ForeignKey("DepartmentId")]
+        //public virtual Department? Department { get; set; }
+    }
+}
