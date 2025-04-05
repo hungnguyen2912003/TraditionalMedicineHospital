d2a56cddff1a1ba6e15a3a6edce26a536c1e884a
hungnguyen2912003 <hungthinh291@gmail.com>
2025-04-05 15:17:45 +0700

    Add props to create user in Employee Tbl

--- Project/Migrations/20250405081627_Add Props to create user in Employee Tbl.Designer.cs
+++ Project/Migrations/20250405081627_Add Props to create user in Employee Tbl.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project.Areas.Admin.Data;
 
@@ -11,9 +12,11 @@ using Project.Areas.Admin.Data;
 namespace Project.Migrations
 {
     [DbContext(typeof(TraditionalMedicineHospitalDbContext))]
-    partial class TraditionalMedicineHospitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250405081627_Add Props to create user in Employee Tbl")]
+    partial class AddPropstocreateuserinEmployeeTbl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Project/Migrations/20250405081627_Add Props to create user in Employee Tbl.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropstocreateuserinEmployeeTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFirstLogin",
+                table: "Employee",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Employee",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFirstLogin",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Employee");
+        }
+    }
+}
