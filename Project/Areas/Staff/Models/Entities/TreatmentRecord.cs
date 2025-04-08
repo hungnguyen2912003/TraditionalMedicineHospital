14173ec6226a999725a717ed08af251433f668d7
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-04-08 23:12:16 +0700

    add treatmentrecord tbl and relationship w patient

---
+++ Project/Areas/Staff/Models/Entities/TreatmentRecord.cs
@@ -0,0 +1,29 @@
+﻿using Project.Models.Commons;
+using Project.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Areas.Staff.Models.Entities
+{
+    [Table("TreatmentRecord")]
+    public class TreatmentRecord : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Diagnosis { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Note { get; set; } = string.Empty;
+        public TreatmentStatus Status { get; set; }
+
+        //Foreign key
+        public Guid PatientId { get; set; }
+
+        /////////////////////////////////////////////////////
+        /// Relationships
+        ///
+        [ForeignKey("PatientId")]
+        public required virtual Patient Patient { get; set; }
+    }
+}
