5bb6d8f30f7fd5f3ce43cb634b4e1508f26aefe1
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-04-09 15:52:57 +0700

    add TreatmentTracking Tbl

--- Project/Areas/Staff/Models/Entities/TreatmentRecordDetail.cs
+++ Project/Areas/Staff/Models/Entities/TreatmentRecordDetail.cs
@@ -12,11 +12,14 @@ namespace Project.Areas.Staff.Models.Entities
         public string? Note { get; set; }
         //Foreign key
         public Guid TreatmentRecordId { get; set; }
+        public Guid TreatmentTrackingId { get; set; }
 
         /////////////////////////////////////////////////////
         /// Relationships
         ///
         [ForeignKey("TreatmentRecordId")]
         public virtual TreatmentRecord TreatmentRecord { get; set; } = null!;
+        [ForeignKey("TreatmentTrackingId")]
+        public virtual TreatmentTracking TreatmentTracking { get; set; } = null!;
     }
 }
