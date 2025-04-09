99c46396dfa75ac22f7521d5b848c25d8ba2136c
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-04-09 15:52:57 +0700

    add TreatmentTracking Tbl

---
+++ Project/Areas/Staff/Models/Entities/TreatmentTracking.cs
@@ -0,0 +1,22 @@
+﻿using Project.Models.Commons;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Areas.Staff.Models.Entities
+{
+    [Table("TreatmentTracking")]
+    public class TreatmentTracking : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [StringLength(10)]
+        public string Code { get; set; } = string.Empty;
+        public DateTime TreatmentDate { get; set; }
+        public string? Note { get; set; }
+
+        /////////////////////////////////////////////////////
+        /// Relationships
+        ///
+        public virtual ICollection<TreatmentRecordDetail> TreatmentRecordDetails { get; set; } = new HashSet<TreatmentRecordDetail>();
+    }
+}
---
+++ Project/Migrations/20250409084407_Initial TreatmentTracking Tbl.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialTreatmentTrackingTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TreatmentTrackingId",
+                table: "TreatmentRecordDetail",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "TreatmentTracking",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    TreatmentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TreatmentTracking", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TreatmentRecordDetail_TreatmentTrackingId",
+                table: "TreatmentRecordDetail",
+                column: "TreatmentTrackingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TreatmentRecordDetail_TreatmentTracking_TreatmentTrackingId",
+                table: "TreatmentRecordDetail",
+                column: "TreatmentTrackingId",
+                principalTable: "TreatmentTracking",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TreatmentRecordDetail_TreatmentTracking_TreatmentTrackingId",
+                table: "TreatmentRecordDetail");
+
+            migrationBuilder.DropTable(
+                name: "TreatmentTracking");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TreatmentRecordDetail_TreatmentTrackingId",
+                table: "TreatmentRecordDetail");
+
+            migrationBuilder.DropColumn(
+                name: "TreatmentTrackingId",
+                table: "TreatmentRecordDetail");
+        }
+    }
+}
