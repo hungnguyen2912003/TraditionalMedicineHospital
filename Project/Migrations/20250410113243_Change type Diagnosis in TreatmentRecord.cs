f46c4d4e4ab62499d9f9554cac0a81875bd9946c
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-04-10 18:33:16 +0700

    Change type Diagnosis in TreatmentRecord

---
+++ Project/Migrations/20250410113243_Change type Diagnosis in TreatmentRecord.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangetypeDiagnosisinTreatmentRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Diagnosis",
+                table: "TreatmentRecord",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Diagnosis",
+                table: "TreatmentRecord",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
