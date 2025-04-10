ee008e568f8c03c730433a5647aa722c0c6d8223
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-04-10 18:33:16 +0700

    Change type Diagnosis in TreatmentRecord

--- Project/Areas/Staff/Models/Entities/TreatmentRecord.cs
+++ Project/Areas/Staff/Models/Entities/TreatmentRecord.cs
@@ -12,7 +12,7 @@ namespace Project.Areas.Staff.Models.Entities
         public Guid Id { get; set; }
         [StringLength(10)]
         public string Code { get; set; } = string.Empty;
-        public string Diagnosis { get; set; } = string.Empty;
+        public DiagnosisType Diagnosis { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Note { get; set; } = string.Empty;
---
+++ Project/Models/Enums/DiagnosisType.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models.Enums
+{
+    public enum DiagnosisType
+    {
+        [Display(Name = "Khí huyết lưỡng hư")]
+        KhiHuyetLuongHu = 0,
+        [Display(Name = "Khí hư")]
+        KhiHu = 1,
+        [Display(Name = "Huyết hư")]
+        HuyetHu = 2,
+        [Display(Name = "Huyết ứ")]
+        HuyetU = 3,
+        [Display(Name = "Khí trệ")]
+        KhiTrap = 4,
+        [Display(Name = "Can dương thượng kháng")]
+        CanDuongThuongKang = 5,
+        [Display(Name = "Tỳ hư")]
+        TyHu = 6,
+        [Display(Name = "Thận dương hư")]
+        ThanDuongHu = 7,
+        [Display(Name = "Thận âm hư")]
+        ThanAmHu = 8,
+        [Display(Name = "Phế nhiệt")]
+        PheNhiet = 9,
+        [Display(Name = "Biểu hàn")]
+        BieuHan = 10,
+        [Display(Name = "Biểu nhiệt")]
+        BieuNhiet = 11,
+        [Display(Name = "Lý hàn")]
+        LyHan = 12,
+        [Display(Name = "Lý nhiệt")]
+        LyNhiet = 13,
+        [Display(Name = "Phong hàn thấp")]
+        PhongHanThap = 14,
+        [Display(Name = "Phong nhiệt thấp")]
+        PhongNhietThap = 15,
+        [Display(Name = "Âm hư nhiệt tích")]
+        AmHuNhietTich = 16,
+        [Display(Name = "Dương hư hàn tích")]
+        DuongHuHanTich = 17
+    }
+}
