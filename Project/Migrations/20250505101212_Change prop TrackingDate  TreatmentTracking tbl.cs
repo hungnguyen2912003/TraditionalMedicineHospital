ec922770835a7b0adb5bd13da823218e7d37ee64
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-05-05 19:25:12 +0700

    change name date in treatmenttracking tbl

---
+++ Project/Migrations/20250505101212_Change prop TrackingDate  TreatmentTracking tbl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangepropTrackingDateTreatmentTrackingtbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TreatmentDate",
+                table: "TreatmentTracking",
+                newName: "TrackingDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TrackingDate",
+                table: "TreatmentTracking",
+                newName: "TreatmentDate");
+        }
+    }
+}
