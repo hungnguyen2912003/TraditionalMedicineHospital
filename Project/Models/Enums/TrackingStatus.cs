a122c4e5cfa6140cdea2617db644dfc0b09182af
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-05-10 16:32:55 +0700

    change enum name in trackingstatus

--- Project/Models/Enums/TrackingStatus.cs
+++ Project/Models/Enums/TrackingStatus.cs
@@ -4,11 +4,11 @@ namespace Project.Models.Enums
 {
     public enum TrackingStatus
     {
-        [Display(Name = "Có khám")]
-        CoKham = 1,
+        [Display(Name = "Có điều trị")]
+        CoDieuTri = 1,
         [Display(Name = "Đã xin phép")]
         XinPhep = 2,
-        [Display(Name = "Không khám")]
-        KhongKham = 3
+        [Display(Name = "Không điều trị")]
+        KhongDieuTri = 3
     }
 }
