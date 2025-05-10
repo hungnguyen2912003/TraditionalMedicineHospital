422f9434c0911b7bf14bb629f161ca18cd87a864
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-05-10 22:53:24 +0700

    add roomtype in room tbl

---
+++ Project/Migrations/20250510145923_Add roomtype in Room Tbl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddroomtypeinRoomTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoomType",
+                table: "Room",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomType",
+                table: "Room");
+        }
+    }
+}
