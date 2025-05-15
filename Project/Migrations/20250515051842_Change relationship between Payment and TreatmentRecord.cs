7c872d600842d50b877c8e7d0eb01d983f1efe5d
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-05-15 12:20:12 +0700

    change repository payment w treatment 1-1

---
+++ Project/Migrations/20250515051842_Change relationship between Payment and TreatmentRecord.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangerelationshipbetweenPaymentandTreatmentRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payment_TreatmentRecordId",
+                table: "Payment");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_TreatmentRecordId",
+                table: "Payment",
+                column: "TreatmentRecordId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payment_TreatmentRecordId",
+                table: "Payment");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_TreatmentRecordId",
+                table: "Payment",
+                column: "TreatmentRecordId");
+        }
+    }
+}
